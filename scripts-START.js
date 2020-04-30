2bec29dbfd011c5891021e79518f8252bb6459c9
morgancostigan <morgan.costigan@gmail.com>
2020-04-30 14:07:01 -0500

    put a setInterval in the timer function to determine seconds left

--- scripts-START.js
+++ scripts-START.js
@@ -3,6 +3,8 @@ function timer(seconds) {
     const then = now + seconds * 1000; //now is in milliseconds, hence the *1000
     // console.log({now, then });
     setInterval(() => {
+        const secondsLeft = (then - Date.now()) / 1000; //dividing by milliseconds to get seconds
+        console.log(secondsLeft);
         
     }, 1000);//end setInterval
     
