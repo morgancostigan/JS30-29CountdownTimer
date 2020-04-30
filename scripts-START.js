02048c851c7fb05dc1e3b56fa629421a291d2a81
morgancostigan <morgan.costigan@gmail.com>
2020-04-30 14:09:38 -0500

    added Math.round to display nice clean whole seconds

--- scripts-START.js
+++ scripts-START.js
@@ -3,11 +3,11 @@ function timer(seconds) {
     const then = now + seconds * 1000; //now is in milliseconds, hence the *1000
     // console.log({now, then });
     setInterval(() => {
-        const secondsLeft = (then - Date.now()) / 1000; //dividing by milliseconds to get seconds
+        const secondsLeft = Math.round((then - Date.now()) / 1000); //dividing by milliseconds to get seconds rounded to the second
         console.log(secondsLeft);
         
     }, 1000);//end setInterval
     
 };//end timer function
 
-timer();
+
