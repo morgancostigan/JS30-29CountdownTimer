f634d366564e7599cadf73c875afec6dbe9431a5
morgancostigan <morgan.costigan@gmail.com>
2020-04-30 14:15:21 -0500

    created a countdown global variable which is used to stop intervals at zero

--- scripts-START.js
+++ scripts-START.js
@@ -1,9 +1,17 @@
+let countdown;
+
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000; //now is in milliseconds, hence the *1000
     // console.log({now, then });
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000); //dividing by milliseconds to get seconds rounded to the second
+        // vvv check if we should stop the function
+        if(secondsLeft <= 0) {
+            clearInterval(countdown);
+            return; //this return is required to prevent -0 after 1
+        }
         console.log(secondsLeft);
         
     }, 1000);//end setInterval
